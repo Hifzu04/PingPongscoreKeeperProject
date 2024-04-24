a489e0fd1333e4631d8e01aea018be9bb98cfc24
Hifzu04 <hifzurbtech@gmail.com>
2024-04-24 11:28:55 +0530

    only styling needs to be done

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -2,13 +2,15 @@ const b1button = document.querySelector('#b1');
 const b2button = document.querySelector('#b2');
 const p1Dispaly = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const reset = document.querySelector('#reset');
+const resetBtn = document.querySelector('#reset');
+const winningScoreTarget =  document.querySelector('#winningScore');    //winning score is winning target
 
 //variable to keep increasing the score
 let p1Score = 0;
 let p2Score = 0;
+//default winning score(target)
+let winningScore= 5;
 
-const winningScore = 5;
 let isGameOver = false;
 
 //when you click on the button1
@@ -17,6 +19,8 @@ b1button.addEventListener('click', function () {
         p1Score += 1;
         p1Dispaly.textContent = p1Score;
          if(p1Score === winningScore){
+            p1Dispaly.classList.add('winner');
+            p2Display.classList.add('loser');
              isGameOver = true;
          }
         
@@ -30,18 +34,36 @@ b2button.addEventListener('click', function () {
         p2Score += 1;
         p2Display.textContent = p2Score;
         if(p2Score === winningScore){
+            p2Display.classList.add('winner');
+            p1Dispaly.classList.add('loser')
             isGameOver = true;
         }
         
     }
 })
 
-// //reset button
-// reset.addEventListener('click' , function(){
-//     isGaveOver= false;
-//     p1Score =0;
-//     p2Score =0;
-//     p1Dispaly = 0 ; 
-//     p2Display =0;
-// })
+
+
+//selection of winning target
+winningScoreTarget.addEventListener('change' , function(){
+    //modify winning target
+    winningScore= parseInt(this.value);
+    //when winning target gets modified , current score shoud also get reset.
+    reset();
+})
+
+//reset button
+resetBtn.addEventListener('click' , reset);
+
+function reset() {
+    isGameOver= false;
+    p1Score =0;
+    p2Score =0;
+    p1Dispaly.textContent = 0; 
+    p2Display.textContent =0;
+    p1Dispaly.classList.remove('winner' ,'loser');
+    p2Display.classList.remove('winner' ,'loser');
+}
+
+
 
